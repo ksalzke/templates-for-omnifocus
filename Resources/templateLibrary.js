9984c10a19f5d30663f6f854a8cc2a707f684a0a
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2020-04-26 21:35:33 +1000

    Create in same folder with string vars replaced

--- Resources/templateLibrary.js
+++ Resources/templateLibrary.js
@@ -1,10 +1,30 @@
-var _ = (function() {
+(() => {
   var templateLibrary = new PlugIn.Library(new Version("1.0"));
 
   templateLibrary.createFromTemplate = () => {
-    console.log("Create From Template");
+    templateToUse = folderNamed("Templates").projectNamed("2019 YE: $Client");
+    parentFolder = folderNamed("Templates");
+
+    replaceTextVariable = (createdProject, variable) => {
+      createdProject.task.apply((tsk) => {
+        tsk.name = tsk.name.replace(variable, "Some String");
+      });
+    };
+
+    // Make a copy of template in specified location
+    createdProject = duplicateSections([templateToUse], parentFolder)[0];
+
+    // Identify variables declared in template task note
+    regex = /(\$.+)*/;
+    variables = templateToUse.task.note.match(regex);
+    if (variables !== null) {
+      variables.forEach((variable) => {
+        replaceTextVariable(createdProject, variable);
+      });
+    }
+
+    // Replace variables
   };
 
   return templateLibrary;
 })();
-_;
