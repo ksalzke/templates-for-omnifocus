963904961f9aa5834c48a815d5be74a77d3c136a
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2020-10-05 15:25:11 +1100

    Adjust dates based on set due/defer

--- Resources/templateLibrary.js
+++ Resources/templateLibrary.js
@@ -2,22 +2,19 @@
   var templateLibrary = new PlugIn.Library(new Version("1.0"));
 
   templateLibrary.createFromTemplate = (template, destination) => {
-    console.log("in createFromTemplate");
     // Create new project
     let newProject = duplicateSections([template], destination)[0];
 
-    // Identify text variables declared in template task note
+    // Identify and replace text variables declared in template task note
     let placeholders = [...newProject.note.matchAll(/«(.*?)»/g)];
     placeholders = placeholders.map((array) => array[1]);
     placeholders.forEach((placeholder) => {
       promptAndReplace(newProject, placeholder);
     });
 
-    // Replace text variables
     function promptAndReplace(project, placeholder) {
       form = new Form();
       form.addField(new Form.Field.String(placeholder, placeholder, null));
-      formPrompt = "Enter value for placeholder:";
       formPromise = form.show("Enter value for placeholder:", "Continue");
 
       formPromise.then(function (formObject) {
@@ -27,6 +24,51 @@
             formObject.values[placeholder]
           );
         });
+      });
+    }
+
+    // Adjust dates
+    function adjustDates(oldDate, newDate, project) {
+      let difference = Calendar.current.dateComponentsBetweenDates(
+        oldDate,
+        newDate
+      );
+
+      project.task.apply((task) => {
+        console.log(task);
+        if (task.dueDate !== null) {
+          task.dueDate = Calendar.current.dateByAddingDateComponents(
+            task.dueDate,
+            difference
+          );
+        }
+        if (task.deferDate !== null) {
+          task.deferDate = Calendar.current.dateByAddingDateComponents(
+            task.deferDate,
+            difference
+          );
+        }
+      });
+    }
+
+    let oldDate = null;
+    if (newProject.dueDate !== null || newProject.deferDate !== null) {
+      let dueForm = new Form();
+      if (newProject.dueDate !== null) {
+        oldDate = newProject.dueDate;
+        dueForm.addField(
+          new Form.Field.Date("newDate", "Due date:", oldDate, null)
+        );
+      } else if (newProject.deferDate !== null) {
+        oldDate = newProject.deferDate;
+        dueForm.addField(
+          new Form.Field.Date("newDate", "Defer date:", oldDate, null)
+        );
+      }
+
+      dueFormPromise = dueForm.show("Date for new project", "Continue");
+      dueFormPromise.then((formObject) => {
+        adjustDates(oldDate, formObject.values["newDate"], newProject);
       });
     }
   };
