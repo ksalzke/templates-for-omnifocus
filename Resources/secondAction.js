7bd65c0ea1188deda5841cfca9d0debfb920aac6
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2020-10-05 12:31:57 +1100

    Add second action so shows in macOS menu (temp)

---
+++ Resources/secondAction.js
@@ -0,0 +1,12 @@
+(() => {
+  var action = new PlugIn.Action(function (selection, sender) {
+    // this action is a placeholder
+  });
+
+  action.validate = function (selection, sender) {
+    // this action is a placeholder
+    return false;
+  };
+
+  return action;
+})();
