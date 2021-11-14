e7ee9c554b51cd3f78de057d779ac9bdcc610892
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2021-11-14 11:17:51 +1100

    feat: use new tab and focus on Mac

--- Templates.omnifocusjs/Resources/goToTemplatesFolder.js
+++ Templates.omnifocusjs/Resources/goToTemplatesFolder.js
@@ -1,13 +1,22 @@
-/* global PlugIn */
+/* global PlugIn Device */
 (() => {
   const action = new PlugIn.Action(async function (selection, sender) {
     const templateLibrary = this.templateLibrary
 
     const templateFolder = await templateLibrary.getTemplateFolder()
 
+    if (Device.current.mac) await document.newTabOnWindow(document.windows[0])
+
     templateFolder.active = true
     const urlStr = 'omnifocus:///folder/' + templateFolder.id.primaryKey
     URL.fromString(urlStr).call(() => {})
+
+    // if user is in focus mode, add the templates folder to the focus
+    // Mac only as focus not yet supported on iOS API
+    if (Device.current.mac) {
+      const focus = document.windows[0].focus
+      if (focus !== null) document.windows[0].focus = [templateFolder]
+    }
   })
 
   action.validate = function (selection, sender) {
